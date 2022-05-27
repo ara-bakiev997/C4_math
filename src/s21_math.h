d4544a591e4a9c52e6f6622f75f43f65297e2686
Tysane Norine <tysaneno@ev-i1.kzn.21-school.ru>
2022-05-27 13:23:58 +0300

    debag

--- src/s21_math.h
+++ src/s21_math.h
@@ -3,19 +3,19 @@
 
 #include <limits.h>
 
-#define S21_NAN 0.0 / 0.0
-#define S21_INFINITY 1.0 / 0.0
-#define S21_MPI 3.14159265358979323846264338327950288
 #define S21_EPS 1e-17
+#define S21_MPI 3.141592653589793238462643383
 #define S21_EPSEQ 1e-07
 #define S21_LN10 2.30258509299404590109
+#define S21_NAN 0.0 / 0.0
+#define S21_INFINITY 1.0 / 0.0
 
-int s21_abs(int x);  // вычисляет абсолютное значение целого числа
-long double s21_acos(double x);  // вычисляет арккосинус
-long double s21_asin(double x);  // вычисляет арксинус
-long double s21_atan(double x);  // вычисляет арктангенс
+int s21_abs(int x);
+long double s21_acos(double x);
+long double s21_asin(double x);
+long double s21_atan(double x);
 long double s21_ceil(double x);  // возвращает ближайшее целое число
-long double s21_cos(double x);  // вычисляет косинус
+long double s21_cos(double x);
 long double s21_exp(double x);  // возвращает e, возведенное в заданную степень
 long double s21_fabs(double x);  // вычисляет абсолютное значение числа
 long double s21_floor(double x);  // возвращает ближайшее число
@@ -23,9 +23,9 @@ long double s21_fmod(double x, double y);  // остаток операции д
 long double s21_log(double x);  // вычисляет натуральный логарифм
 long double s21_pow(double base,
                     double exp);  // возводит число в заданную степень
-long double s21_sin(double x);  // вычисляет синус
+long double s21_sin(double x);
 long double s21_sqrt(double x);  // вычисляет квадратный корень
-long double s21_tan(double x);  // вычисляет тангенс
+long double s21_tan(double x);
 
 //  dop func
 int check_nan(double x);
